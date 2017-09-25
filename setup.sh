d52923f0e95747553122c76afaef35be3d72b94c
kangdongho <kangd@ethz.ch>
2017-09-25 18:42:17 +0200

    setup.sh edited for several issues

--- setup.sh
+++ setup.sh
@@ -5,11 +5,12 @@ echo installing vundle...
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # zsh and oh-my-zsh
-echo installing zsh...
+echo installing zsh and oh-my-zsh
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # ubuntu
-    apt install zsh 
+    sudo apt install curl
+    sudo apt install zsh 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # mac os
     brew install zsh zsh-completions
@@ -27,10 +28,17 @@ else
     echo unknown os. do not install zsh.
 fi
 
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # softlink 
 echo copying config files to home directory...
 
 # TODO change path
+rm ~/.profile
+rm ~/.vimrc
+rm ~/.zshrc
+
 ln -s ~/git/config-files/.profile ~/.profile
 ln -s ~/git/config-files/.vimrc ~/.vimrc
 ln -s ~/git/config-files/.zshrc ~/.zshrc
@@ -40,8 +48,8 @@ echo installing ...
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # ubuntu
-    apt-get install vim ctags
-    apt-get install build-essentials terminator cmake
+    sudo apt-get install vim ctags
+    sudo apt-get install build-essentials terminator cmake
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # mac os
     brew install vim ctags 
